f5bc23b0fea0c6bb6b333efa059a7debd18b67e3
skong <sean.k@live.com>
2023-10-07 16:06:43 +0800

    update

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1,55 +1,66 @@
 #include "ft_printf.h"
 
+int ft_print_arg(char c, va_list arg)
+{
+	int count; 
+
+	count = 0; 
+	if (c == %)
+		count += ft_print.c('c')
+	else if(c == 'c')
+		count += ft_print_c(va_arg(arg, int)); 
+	else if(c == 's')
+		count += ft_print_s(va_arg(arg, const char *));
+	else if(c =='p')
+		count += ft_print_hex(va_arg(arg, void *));
+	else if(c == 'd' || c =='i')
+		count += ft_print_int(va_args(arg, int)); 
+	else if(c == 'u')
+		count += ft_print_unsigned(va_args(arg, int)); 
+	return (count);
+}
+
+
+int ft_parse(char *str, va_list arg)
+{
+	int i;
+	int count;
+
+	i = 0
+	count = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '%' str[i + 1] !='\'0')
+		{
+			i++;
+			if (ft_istype(str[i]))
+				count = count + ft_print_arg(str[i], arg);
+			else if (str[i] != '\0')
+				count = count + ft_print_c(str[i]);
+				i++;
+		}
+		else
+			count = count + ft_print_c(str[i]);
+			i++;
+	}
+	return (count);
+}
+			
+			
+
 int	ft_printf(const char *format, ...)
 {
-	va_list	arg;
-	int	printed_char;
-	char	value_c;
-	int	value_id;
-	void	value_p;
-	char	*value_s;
-	char	buffer[32];
+	va_list	args;
+	int 	count; 
+	char	*str;
 
-	printed_char = 0;
-	va_start(arg, format);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				value_c = (char) va_arg(arg, int);
-				ft_putchar (value_C);
-				printed_char++;
-				break;
-			}
-			else if (*format == 's')
-			{
-				value_s = va_arg(args, char*);
-				ft_putstr(value_s);
-				print_char += ft_strlen(value_s);
-				break;
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				value_id = va_arg(arg, int);
-				ft_putstr(ft_itoa(value_I));
-				printed_char += ft_strlen(ft_itoa(value_I));
-				break;
-			}
-			else if (*format == 'p')
-			{
-				value_p = va_arg(arg, void*);
-			}
-			else 
-			{	
-				ft_putchar(format);
-				printed_chars++;
-			}
-		}
-		format++;
-	} 
-	va_end(args);
-	return (printed_chars);
+	if (!format || *format  == '\0')
+		return (0); 
+	str = ft_strdup(format);
+	count = 0; 
+	va_start(args, format); 
+	count = ft_parse(str, args);
+	va_end(args); 
+	free(str);
+	return (count); 
 }
