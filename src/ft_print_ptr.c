14f20b0c5916b817cbd268de8274705b2ba9fc62
Sean Kong <skong@c2r3s12.42singapore.sg>
2023-10-13 16:45:44 +0800

    updated for NULL and 0

--- src/ft_print_ptr.c
+++ src/ft_print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: skong <skong@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:05:46 by skong             #+#    #+#             */
-/*   Updated: 2023/10/12 12:24:00 by skong            ###   ########.fr       */
+/*   Updated: 2023/10/13 16:37:24 by skong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_print_ptr(void *ptr)
 	int	count;
 
 	count = 0;
+	if (!ptr)
+		return (ft_print_str("(nil)"));
 	ft_print_str("0x");
 	count += 2;
 	count += ft_print_hex_l((unsigned long int) ptr);
--- src/ft_print_str.c
+++ src/ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: skong <skong@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:07:10 by skong             #+#    #+#             */
-/*   Updated: 2023/10/12 12:24:35 by skong            ###   ########.fr       */
+/*   Updated: 2023/10/13 16:33:15 by skong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
@@ -16,6 +16,8 @@ int	ft_print_str(const char *str)
 	int	count;
 
 	count = 0;
+	if (!str)
+		return (ft_print_str("(null)")); 
 	while (*str)
 	{
 		ft_putchar(*str);
